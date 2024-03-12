86d5092c70a2af003d5d69807f9376c5753aa617
RiccardoMarino00 <riccardo.marino09@gmail.com>
2024-03-12 17:47:59 +0100

    Added: comment

--- js/pali.js
+++ js/pali.js
@@ -13,21 +13,22 @@ for (let index = lastIndex; index >= 0; index--){
     console.log(reverseWord);
    
 }
-// Creo una funzione per capire se la parola inserita è palindroma (cioè se la parola inserita dall'utente anche se letta al contrario è uguale)
-  // La funzione deve ritornare true se la parola è palindroma
-  // Dev ritornare false se la parola non è palindroma
-// Stampare in console un messaggio per dire all'utente se la parola che ha inserito è palindroma oppure no
 
 const result = wordPalindrome(wordUser);
 console.log(result)
 
+// Creo una funzione per capire se la parola inserita è palindroma (cioè se la parola inserita dall'utente anche se letta al contrario è uguale)
 function wordPalindrome (wordUser){
-
+    // La funzione deve ritornare true se la parola è palindroma
     if (wordUser === reverseWord){
         return true
+    // Dev ritornare false se la parola non è palindroma
     } else {
         return false
     }
 
 }
+
+// Stampare in console un messaggio per dire all'utente se la parola che ha inserito è palindroma oppure no
+
  
